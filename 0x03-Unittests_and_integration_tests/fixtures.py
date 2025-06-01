29896069bd213ea662c5f01a82355e3804c03fe5
EugineI <eugineindimuli@gmail.com>
2025-06-01 20:48:53 +0300

    v3

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python3
+org_payload = {
+    "login": "google",
+    "id": 1,
+    "url": "https://api.github.com/orgs/google",
+    "repos_url": "https://api.github.com/orgs/google/repos"
+}
 
+repos_payload = [
+    {
+        "id": 1,
+        "name": "repo1",
+        "license": {"key": "apache-2.0"}
+    },
+    {
+        "id": 2,
+        "name": "repo2",
+        "license": {"key": "mit"}
+    },
+    {
+        "id": 3,
+        "name": "repo3",
+        "license": None
+    }
+]
+
+expected_repos = ["repo1", "repo2", "repo3"]
+
+apache2_repos = ["repo1"]
 TEST_PAYLOAD = [
   (
     {"repos_url": "https://api.github.com/orgs/google/repos"},
