68d465ee91d6b06182ca39b6f0eacb76d8397beb
EugineI <eugineindimuli@gmail.com>
2025-06-01 19:29:39 +0300

    correction2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ test utils"""
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 from utils import access_nested_map
 from utils import get_json
@@ -16,28 +16,27 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """test matches """
+        """test correct access """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
         @parameterized.expand([
             ({}, ("a",)),
             ({"a": 1}, ("a", "b")), ])
         def test_access_nested_map_exception(self, nested_map, path):
-            """ map exception"""
+            """test exception raised"""
             with self.assertRaises(KeyError) as context:
                 access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception),
-                         repr(path[len(context.exception.args[0]):][0]))
+            self.assertEqual(str(context.exception), repr(path[-1]))
 
 
 class TestGetJson(unittest.TestCase):
-    """ test json """
+    """ test json fetching"""
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
     def test_get_json(self, test_url, test_payload):
-        """test get json method"""
+        """test get_json method with mock"""
         mock_response = Mock()
         mock_response.json.return_value = test_payload
 
@@ -71,3 +70,7 @@ class TestMemoize(unittest.TestCase):
             self.assertEqual(result1, 42)
             self.assertEqual(result2, 42)
             mck_mthd.assert_called_once()
+
+
+if __name__ == '__main__':
+    unittest.main()
